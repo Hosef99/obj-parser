e82aa0a463f152850e47c807d5afa237e8717a4d
Hosef <joseph.txh@gmail.com>
2026-05-14 21:13:33 +0800

    Removed debug message

--- src/parser.cpp
+++ src/parser.cpp
@@ -146,10 +146,6 @@ namespace OP
 
         obj_file.close();
 
-        std::cout << "Size of vertices: " << vertices.size() << std::endl;
-        for (int i = 0; i < vertices.size(); i++)
-            std::cout << vertices[i].position << " " << vertices[i].uv << " " << vertices[i].normal << std::endl;
-
         return vertices;
     }
 }
